d94f5158b2a2207ca182ce24125fb92db24a34b3
Vlad Vernyhora <darkforest.v@gmail.com>
2023-11-16 07:30:30 +0200

    method checkKey added

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -8,10 +8,12 @@ public class Main {
 
         checkCommand(args);
         checkFilePath(args);
+        checkKey(args);
 
     }
 
     public static void checkCommand(String[] args) { //Перевіряємо чи обрана одна з 3х існуючих команд
+
         if (!(args[0].equalsIgnoreCase("encrypt")) &&
                 !(args[0].equalsIgnoreCase("decrypt")) &&
                 !(args[0].equalsIgnoreCase("brute-force"))) {
@@ -30,5 +32,24 @@ public class Main {
         }
 
     }
+
+    public static void checkKey(String[] args) { //Перевіряємо чи підходить нам ключ
+
+        try {
+
+            if (Integer.parseInt(args[2]) < 0) {
+
+                throw new IllegalArgumentException("Invalid key \"" + args[2] + "\". It has to be greater than 0");
+
+            }
+
+        } catch (NumberFormatException e) {
+
+            throw new IllegalArgumentException("Invalid key \"" + args[2] + "\". It has to be integer");
+
+        }
+
+    }
+
 
 }
