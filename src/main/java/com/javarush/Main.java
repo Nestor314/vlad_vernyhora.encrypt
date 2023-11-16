dea6ecb23ae70eaf1d2a59bd906ec96d3d56bd15
Vlad Vernyhora <darkforest.v@gmail.com>
2023-11-16 07:15:35 +0200

    method checkFilePath added + test.txt file filled with text added

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -1,13 +1,17 @@
 package com.javarush;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
 
         checkCommand(args);
+        checkFilePath(args);
 
     }
 
-    public static void checkCommand(String[] args) {
+    public static void checkCommand(String[] args) { //Перевіряємо чи обрана одна з 3х існуючих команд
         if (!(args[0].equalsIgnoreCase("encrypt")) &&
                 !(args[0].equalsIgnoreCase("decrypt")) &&
                 !(args[0].equalsIgnoreCase("brute-force"))) {
@@ -15,6 +19,16 @@ public class Main {
             throw new IllegalArgumentException("Wrong command \"" + args[0] + "\"");
 
         }
+    }
+
+    public static void checkFilePath(String[] args) { //Перевіряємо чи дійсний шлях до файлу
+
+        if (!Files.exists(Paths.get(args[1])) || !Files.isRegularFile(Paths.get(args[1]))) {
+
+            throw new IllegalArgumentException("Invalid file path \"" + args[1] + "\"");
+
+        }
+
     }
 
 }
